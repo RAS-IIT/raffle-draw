71c45d0a09cdd7e47406756820f5326c791c0fc7
Sahan Dakshitha <sahandakshitha45@gmail.com>
2024-09-15 23:35:21 +0530

    Updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import data from './data.js';
+import React, { useState } from 'react';
 
 const App = () => {
+  const [names, setNames] = useState(data);
   return (
     <div>App</div>
   )

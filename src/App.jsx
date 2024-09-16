5bbdffb411a8c6cc394c44a8becb9c1add7c2d90
Sahan Dakshitha <sahandakshitha45@gmail.com>
2024-09-16 10:18:13 +0530

    Added a shuffle function

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import data from './data.js';
+import { shuffle } from 'lodash';
 import React, { useEffect, useState } from 'react';
 
 const App = () => {
   const [names, setNames] = useState(data);
+  const [initialLoad, setInitialLoad] = useState(false);
 
   function startRaffle(){
     if(names.length <= 1){
@@ -11,20 +13,22 @@ const App = () => {
     const randomIndex = Math.floor(Math.random() * names.length);
     const filterOutNames = names.filter((name) => name !== names[randomIndex]);
     setNames(filterOutNames);
+    setInitialLoad(true);
   }
 
   useEffect(() => {
+    if(initialLoad){
     const filteringTimer = setTimeout(() => {
       startRaffle();
-    }, 1000)
+    }, 1000);
     return() => clearTimeout(filteringTimer);
-  }, [names, startRaffle])
+  }
+  }, [names, startRaffle, initialLoad])
 
   return (
     <div>
-      <button onClick={startRaffle}>
-        Start Raffle
-      </button>
+      <button onClick={startRaffle}>Start Raffle</button>
+      <button onClick={() => setNames(shuffle(names))}>Shuffle</button>
       {names.map((names, index) => (
         <div key={index}>
           <ul>

1d2f27308e4308339212033df1025626fda35ea2
Sahan Dakshitha <sahandakshitha45@gmail.com>
2024-09-16 09:57:35 +0530

    Added function to remove randomly selected

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,28 @@ import React, { useState } from 'react';
 
 const App = () => {
   const [names, setNames] = useState(data);
+
+  function startRaffle(){
+    if(names.length <= 1){
+      return
+    }
+    const randomIndex = Math.floor(Math.random() * names.length);
+    const filterOutNames = names.filter((name) => name !== names[randomIndex]);
+    setNames(filterOutNames);
+  }
   return (
-    <div>App</div>
+    <div>
+      <button onClick={startRaffle}>
+        Start Raffle
+      </button>
+      {names.map((names, index) => (
+        <div key={index}>
+          <ul>
+          <li>{names.name}</li>
+          </ul>
+        </div>
+      ))}
+    </div>
   )
 }
 
